af9db541654eeb70b62fca5aa97bfde259d43b3b
getinthere <getinthere@naver.com>
2025-04-01 14:05:04 +0900

    서버준비

--- src/main/java/shop/mtcoding/blog/board/BoardApiController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardApiController.java
@@ -1,10 +1,7 @@
 package shop.mtcoding.blog.board;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,22 +12,22 @@ public class BoardApiController {
     private final BoardRepository boardRepository;
 
     @PutMapping("/api/boards/{id}")
-    public ApiUtil<?> update(@PathVariable int id, @RequestBody BoardRequest.UpdateDTO requestDTO){
+    public ApiUtil<?> update(@PathVariable int id, @RequestBody BoardRequest.UpdateDTO requestDTO) {
         boardRepository.updateById(requestDTO, id);
         return new ApiUtil<>(null);
     }
 
 
     @PostMapping("/api/boards")
-    public ApiUtil<?> write(@RequestBody BoardRequest.WriteDTO requestDTO){
+    public ApiUtil<?> write(@RequestBody BoardRequest.WriteDTO requestDTO) {
         boardRepository.insert(requestDTO);
         return new ApiUtil<>(null);
     }
 
     @DeleteMapping("/api/boards/{id}")
-    public ApiUtil<?> deleteById(@PathVariable Integer id, HttpServletResponse response){
+    public ApiUtil<?> deleteById(@PathVariable Integer id, HttpServletResponse response) {
         Board board = boardRepository.selectOne(id);
-        if(board == null){
+        if (board == null) {
             response.setStatus(404);
             return new ApiUtil<>(404, "해당 게시글을 찾을 수 없습니다");
         }
@@ -38,6 +35,7 @@ public class BoardApiController {
         boardRepository.deleteById(id);
         return new ApiUtil<>(null);
     }
+
     @GetMapping("/api/boards/{id}")
     public ApiUtil<?> findById(@PathVariable int id) {
         Board board = boardRepository.selectOne(id);
@@ -46,7 +44,6 @@ public class BoardApiController {
 
     @GetMapping("/api/boards")
     public ApiUtil<?> findAll(HttpServletResponse response) {
-        //response.setStatus(200);
         List<Board> boardList = boardRepository.selectAll();
         return new ApiUtil<>(boardList); // MessageConverter
     }
