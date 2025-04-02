aa12aeea5c5b69b4ab3b1b6e5793c16baf6cbfdc
yh88888888 <yh88888888jps@gmail.com>
2025-04-02 17:04:51 +0900

    AJAX 통신

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -1,5 +1,8 @@
 package shop.mtcoding.blog.board;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +23,12 @@ public class BoardController {
     }
 
     @GetMapping("/board/{id}/updateForm")
-    public String updateForm(@PathVariable int id) {
+    public String updateForm(@PathVariable("id") int id, HttpServletRequest request, HttpServletResponse response){
+        Cookie cookie = new Cookie("boardId", id+"");
+        cookie.setHttpOnly(true);//서버가 브라우저에 주고 가져오는 것만 허용, 중간에 자바스크립트가 가져가는걸 용납 안함
+        response.addCookie(cookie);
+        //응답의 헤더 → Set-Cookie : boardId=5
+        request.setAttribute("boardId", id);
         return "board/updateForm";
     }
 }
